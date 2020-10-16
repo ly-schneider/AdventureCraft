014a92c45638709ba8be94b943dcc677b8c81dcf
ly-schneider <71436505+ly-schneider@users.noreply.github.com>
2020-10-16 15:05:44 +0200

    Create game.js

---
+++ game.js
@@ -0,0 +1,29 @@
+//Create a Pixi Application
+let app = new PIXI.Application({ 
+    width: 256, 
+    height: 256,                       
+    antialias: true, 
+    transparent: false, 
+    resolution: 1
+  }
+);
+
+let catimageurl = "https://raw.githubusercontent.com/kittykatattack/learningPixi/master/examples/images/cat.png";
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+//load an image and run the `setup` function when it's done
+PIXI.loader
+  .add(catimageurl)
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+  //Create the cat sprite
+  let cat = new PIXI.Sprite(PIXI.loader.resources[catimageurl].texture);
+  
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
